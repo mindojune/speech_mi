63cdd168e2db01a49f6a50585feb2389fa8b8121
Do June Min <dojmin@gl-login3.arc-ts.umich.edu>
2024-11-01 15:06:01 -0400

    good, now TODO is exploratory analysis first then replication on other data

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # Speech MI 2024 Fall Project
 
+## TODOs
+So for classification on annomi, speech helps. 
+The next step is to do:
+1. exploratory analysis
+    - on how speech helps
+    - finegrained analysis
+        - for example confusion matrix on the categories
+        
+2. do experiments on forecasting and other datasets.
 
-## To train forecasting model on AnnoMI Dataset 
+
+## Related Work
+- [Multimodal Automatic Coding of Client Behavior in Motivational Interviewing](https://dl.acm.org/doi/pdf/10.1145/3382507.3418853) 
+- 
+
+## Instructions
+
+### To train forecasting model on AnnoMI Dataset 
 `python run_experiment.py --mode train --modality speech --run_name speech_241101_lr1e4_frozen_eos_padding_bug_fixed \
     --batch_size 2  --test_batch_size 4  --datatype float16 --steps 200000 --data_length -1 -1 -1 \
     --validation_interval 2000  --learning_rate 1e-4 --grad_accum_interval 64 --freeze_encoder --use_lora --use_audio_eos \
     --task forecasting --dataset annomi`
 
-## To test forecasting model on AnnoMI Dataset 
+### To test forecasting model on AnnoMI Dataset 
 `speech_checkpoint_path=your_saved_model_path`
 
 `speech_lora_checkpoint_path=your_saved_lora_path`
--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # 2 / 18000
 
-
+##################################################################
+# annomi classification speech [trained] speech_241031_lr1e4_frozen_eos_padding_bug_fixed
 epoch=9
 step=34000 #40000 #50000
-runname=speech_epoch_${epoch}_step_${step}
+runname=speech_epoch_${epoch}_step_${step}_classification
 savename=speech_241031_lr1e4_frozen_eos_padding_bug_fixed #cont_speech_epoch_5_step_38000 #speech_241030_lr5e5_encoderonly #speech_test_241029_leftpadded # speech_241030_lr1e4_frozen
 checkname=checkpoint_epoch_${epoch}_step_${step}.pt
 loraname=lora_checkpoint_epoch_${epoch}_step_${step}
@@ -17,25 +18,61 @@ python run_experiment.py --mode test --modality speech \
     --checkpoint_path $speech_checkpoint_path \
     --lora_checkpoint_path $speech_lora_checkpoint_path --use_lora  --use_audio_eos
 
-
-#################################
-epoch=9 #14
-step=34000 #50000
-runname=speech_epoch_${epoch}_step_${step}
-savename=speech_241031_lr5e5_frozen #speech_241030_lr5e5_encoderonly #speech_test_241029_leftpadded # speech_241030_lr1e4_frozen
+##################################################################
+# annomi classification text [trained]
+epoch=11 #9
+step=38000 #34000 #40000 #50000
+runname=text_epoch_${epoch}_step_${step}_classification
+savename=text_241101_lr1e4_frozen_eos_padding_bug_fixed #cont_speech_epoch_5_step_38000 #speech_241030_lr5e5_encoderonly #speech_test_241029_leftpadded # speech_241030_lr1e4_frozen
 checkname=checkpoint_epoch_${epoch}_step_${step}.pt
 loraname=lora_checkpoint_epoch_${epoch}_step_${step}
 speech_checkpoint_path=/scratch/mihalcea_owned_root/mihalcea_owned1/dojmin/speech_mi_logs/classification_experiment/${savename}/${checkname}
 speech_lora_checkpoint_path=/scratch/mihalcea_owned_root/mihalcea_owned1/dojmin/speech_mi_logs/classification_experiment/${savename}/${loraname}
 
+python run_experiment.py --mode test --modality text \
+    --run_name test_${runname} --test_batch_size 2 \
+    --datatype float16 --steps 50000 --data_length -1 -1 -1 --validation_interval 2000 \
+    --lora_checkpoint_path $speech_lora_checkpoint_path --use_lora  --use_audio_eos
+
+
+##################################################################
+# annomi forecasting speech [training]
+epoch=5
+step=20000 #40000 #50000
+runname=speech_epoch_${epoch}_step_${step}_forecasting
+savename=speech_241101_lr1e4_frozen_eos_padding_bug_fixed #cont_speech_epoch_5_step_38000 #speech_241030_lr5e5_encoderonly #speech_test_241029_leftpadded # speech_241030_lr1e4_frozen
+checkname=checkpoint_epoch_${epoch}_step_${step}.pt
+loraname=lora_checkpoint_epoch_${epoch}_step_${step}
+speech_checkpoint_path=/scratch/mihalcea_owned_root/mihalcea_owned1/dojmin/speech_mi_logs/forecasting_experiment/${savename}/${checkname}
+speech_lora_checkpoint_path=/scratch/mihalcea_owned_root/mihalcea_owned1/dojmin/speech_mi_logs/forecasting_experiment/${savename}/${loraname}
+
 python run_experiment.py --mode test --modality speech \
-    --run_name test_${runname} --test_batch_size 1 \
+    --run_name test_${runname} --test_batch_size 2 \
     --datatype float16 --steps 50000 --data_length -1 -1 -1 --validation_interval 2000 \
     --checkpoint_path $speech_checkpoint_path \
-    --lora_checkpoint_path $speech_lora_checkpoint_path --use_lora 
+    --lora_checkpoint_path $speech_lora_checkpoint_path --use_lora  --use_audio_eos --task forecasting
 
-
+##################################################################
+# annomi forecasting text [training]
 #################################
+epoch=5
+step=18000 #40000 #50000
+runname=text_epoch_${epoch}_step_${step}_forecasting
+savename=text_241101_lr1e4_frozen_eos_padding_bug_fixed #cont_speech_epoch_5_step_38000 #speech_241030_lr5e5_encoderonly #speech_test_241029_leftpadded # speech_241030_lr1e4_frozen
+checkname=checkpoint_epoch_${epoch}_step_${step}.pt
+loraname=lora_checkpoint_epoch_${epoch}_step_${step}
+speech_checkpoint_path=/scratch/mihalcea_owned_root/mihalcea_owned1/dojmin/speech_mi_logs/forecasting_experiment/${savename}/${checkname}
+speech_lora_checkpoint_path=/scratch/mihalcea_owned_root/mihalcea_owned1/dojmin/speech_mi_logs/forecasting_experiment/${savename}/${loraname}
+
+python run_experiment.py --mode test --modality text \
+    --run_name test_${runname} --test_batch_size 2 \
+    --datatype float16 --steps 50000 --data_length -1 -1 -1 --validation_interval 2000 \
+    --lora_checkpoint_path $speech_lora_checkpoint_path --use_lora  --use_audio_eos --task forecasting
+
+
+##################################################################
+##################################################################
+##################################################################
 epoch=14
 step=100000
 runname=speech_epoch_${epoch}_step_${step}
